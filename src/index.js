bc20c32ae0716a5fa153904f3511aec8b33cb0e4
rajmaravanthe <rraajjpoojary@gmail.com>
2018-10-06 20:05:46 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import './index.css';
+import App from './App';
+
+const app = (
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
